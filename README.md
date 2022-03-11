4e2d98625e6f89d506a921018f9b2c2773517a3b
Joseph Manning <jgmanning3@yahoo.com>
2022-03-11 16:38:55 -0700

    Add styling and create README

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # imgix-frontend-interface
+
+
+## Description
+
+A React frontend interface using imgix image assets to access their API to add a color overlay and personalized text to the image.
+
+
+## Usage
+
+In order to use the React interface, you will need to utilize public image URL assets from imgix. The available URLs you can input in the "Image URL" field are:
+- https://assets.imgix.net/examples/butterfly.jpg
+- https://assets.imgix.net/unsplash/bridge.jpg
+- https://assets.imgix.net/unsplash/pineneedles.jpg
+- https://assets.imgix.net/unsplash/motorbike.jpg
+- https://assets.imgix.net/unsplash/mountains.jpg
+
+
+## Questions
+
+If you have any questions, I can be reached on my GitHub at [JMan4342](https://github.com/JMan4342).
--- src/Components/SubmitInput.js
+++ src/Components/SubmitInput.js
@@ -5,34 +5,27 @@ const SubmitInput = ({ onSendInput }) => {
     myImageUrl: "",
     myColorOverlay: "#111111",
     myText: "",
-    // myTextAlign: "",
   };
 
   const [formInput, setFormInput] = useState(clearData);
-  // const [myImage, setMyImage] = useState("");
 
   const submitFormData = () => {
     const imageInfo = {
       myImageUrl: formInput.myImageUrl,
       myColorOverlay: formInput.myColorOverlay.slice(1),
       myText: formInput.myText,
-      // myTextAlign: formInput.myTextAlign,
     };
-    console.log({ imageInfo });
     onSendInput(imageInfo);
-    // setMyImage(imageInfo) = `${imageInfo.myImageUrl}?txt=${imageInfo.myText}&blend=${imageInfo.myColorOverlay}`;
-    // console.log({ myImage });
     setFormInput(clearData);
   };
 
   return (
     <div className="form">
-      <form>
+      <form className="formInput">
         <label>Enter Your Image URL</label>
         <br></br>
         <input
           type="text"
-          className="search"
           placeholder="Image URL"
           name="myImageUrl"
           value={formInput.myImageUrl}
@@ -41,12 +34,11 @@ const SubmitInput = ({ onSendInput }) => {
           }}
         />
       </form>
-      <form>
+      <form className="formInput">
         <label>Select Your Overlay Color</label>
         <br></br>
         <input
           type="color"
-          className="search"
           name="myColorOverlay"
           value={formInput.myColorOverlay}
           onChange={(event) => {
@@ -54,12 +46,11 @@ const SubmitInput = ({ onSendInput }) => {
           }}
         />
       </form>
-      <form>
+      <form className="formInput">
         <label>Enter Your Personalized Text</label>
         <br></br>
         <input
           type="text"
-          className="search"
           placeholder="Personalized Text"
           name="myText"
           value={formInput.myText}
