3d53c2896824d1a4b64cdaf714c13973314ce846
Joseph Manning <jgmanning3@yahoo.com>
2022-03-11 11:13:59 -0700

    Init commit, start build of interface form

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { useState } from "react";
+import SearchForm from "./Components/SearchForm";
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Create Your imgix Image</h1>
+      <SearchForm />
+    </div>
+  );
+}
+
+export default App;
